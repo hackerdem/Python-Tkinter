7c90b9f4e02ddb03cc18ffa3cda89328d3c77c48
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-10-21 20:06:34 +1100

    new components

---
+++ login.py
@@ -0,0 +1,28 @@
+from tkinter import *
+root=Tk()
+#delete show when added!!!!
+def show():
+    
+    print("You entered:")
+    print("Employee Number:"+str(employee_number.get()))
+    print("Login Password:"+password.get())
+    print("Remember Me:"+str(remember_me.get()))
+    print('*'*30)
+
+Label(root,text="Emloyee Number:").grid(row=1,column=1)
+employee_number=IntVar()
+Entry(root,width=40,textvariable=employee_number).grid(row=1,column=2,columnspan=2)
+employee_number.set("123456")
+
+Label(root,text="Login Password:").grid(row=2,column=1,sticky='w')
+password=StringVar()
+Entry(root,width=40,show='*',textvariable=password).grid(row=2,column=2,columnspan=2)
+password.set("mysecretpassword")
+
+Button(root,text='Login',command=show).grid(row=3,column=3)
+
+remember_me=BooleanVar()
+Checkbutton(root,text='Remember Me',variable=remember_me).grid(row=3,column=2)
+remember_me.set(True)
+
+root.mainloop()
