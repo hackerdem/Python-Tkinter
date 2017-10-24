42d3b751ac9cff77c10474df19a54c8d2b0f03b6
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-10-24 14:23:34 +1100

    gui completed without full functions

--- text_editor.py
+++ text_editor.py
@@ -1,15 +1,28 @@
 from tkinter import *
+def cut():
+    content_text.event_generate("<<Cut>>")
 PROGRAM_NAME="Footprint Editor"
-
 root=Tk()
 root.geometry('350x350')
 root.title(PROGRAM_NAME)
+
 #icons
 #WARNINIG##########change ico is not working make it gif
 """new_file_icon=PhotoImage(file='if_file_173011.ico')
 saveas_file_icon=PhotoImage(file='if_Save-as_85541.ico')
 save_icon=PhotoImage(file='if_save_173091.ico')
 open_file_icon=PhotoImage(file='if_folder-open_173017.ico')"""
+
+""" Color options are listed in a dictionary"""
+color_shemes={
+    'Default': '#000000.#FFFFFF',
+    'Greygarious': '#83406A.#D1D4D1',
+    'Aquamarine': '#5B8340.#D1E7E0',
+    'Bold Beige': '#4B4620.#FFF0E1',
+    'Cobalt Blue': '#ffffBB.#3333aa',
+    'Olive Green': '#D1E7E0.#5B8340',
+    'Night Mode': '#FFFFFF.#000000',
+    }
 
 #menu bar
 menu_bar=Menu(root)
@@ -28,7 +41,7 @@ edit_menu=Menu(menu_bar,tearoff=0)
 edit_menu.add_command(label='Undo',accelerator='Ctrl+Z',compound='left',image='',underline=0)
 edit_menu.add_command(label='Redo',accelerator='Ctrl+Y',compound='left',image='',underline=0)
 edit_menu.add_separator()
-edit_menu.add_command(label='Cut',accelerator='Ctrl+X',compound='left',image='',underline=0)
+edit_menu.add_command(label='Cut',accelerator='Ctrl+X',command=cut,compound='left',image='',underline=0)
 edit_menu.add_command(label='Copy',accelerator='Ctrl+C',compound='left',image='',underline=0)
 edit_menu.add_command(label='Paste',accelerator='Ctrl+V',compound='left',image='',underline=0)
 edit_menu.add_separator()
@@ -36,7 +49,24 @@ edit_menu.add_command(label='Find',accelerator='Ctrl+F',compound='left',image=''
 file_menu.add_separator()
 edit_menu.add_command(label='Select All',accelerator='Ctrl+A',compound='left',image='',underline=0)
 menu_bar.add_cascade(label='Edit',menu=edit_menu)
+
+#view menu
 view_menu=Menu(menu_bar,tearoff=0)
+show_line_number=IntVar()
+show_line_number.set(1)
+view_menu.add_checkbutton(label="Show Line Number",variable=show_line_number)
+show_cursor_info=IntVar()
+show_cursor_info.set(1)
+view_menu.add_checkbutton(label="Show Cursor Location at Bottom",variable=show_cursor_info)
+highlight_line=IntVar()
+view_menu.add_checkbutton(label="Highlight Current Line",onvalue=1,offvalue=0,variable=highlight_line)
+themes_menu=Menu(menu_bar,tearoff=0)
+view_menu.add_cascade(label="Themes",menu=themes_menu)
+theme_choice=StringVar()
+theme_choice.set('Default')
+for i in sorted(color_shemes):
+    themes_menu.add_radiobutton(label=i,variable=theme_choice)
+
 menu_bar.add_cascade(label='View',menu=view_menu)
 
 #about menu
@@ -47,11 +77,19 @@ menu_bar.add_cascade(label='About',menu=about_menu)
 root.config(menu=menu_bar)
 
 #shortcut bar
-shortcut_bar=Frame(root,height=25,background='grey')
+shortcut_bar=Frame(root,height=25,background='#DCDCDC')
 shortcut_bar.pack(expand='no',fill='x')
 
 # line number bar
-line_number_bar=Text(root,width=4,padx=3,takefocus=0,border=0,background='blue',state='disabled',wrap='none')
+line_number_bar=Text(root,width=4,padx=3,takefocus=0,border=0,background='#778899',state='disabled',wrap='none')
 line_number_bar.pack(side='left',fill='y')
+
+#text and scroolbar widgets for root window
+content_text=Text(root,wrap='word',background='#778899')
+content_text.pack(expand='yes',fill='both')
+scroll_bar=Scrollbar(content_text)
+content_text.configure(yscrollcommand=scroll_bar.set)
+scroll_bar.config(command=content_text.yview)
+scroll_bar.pack(side='right',fill='y')
 
 root.mainloop()
