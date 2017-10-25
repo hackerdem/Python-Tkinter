07f98961d7328089bedbaae6136fcf4b50b392c7
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-10-25 14:58:08 +1100

    bug fixes

--- text_editor.py
+++ text_editor.py
@@ -15,25 +15,36 @@ def find_text(event=None):
     search_toplevel=Toplevel(root)
     search_toplevel.title('Find Text')
     search_toplevel.transient(root)
-    search_toplevel.reseizable(False,False)
+    search_toplevel.resizable(False,False)
     Label(search_toplevel,text="Find All:").grid(row=0,column=0,sticky='e')
     search_entry_widget=Entry(search_toplevel,width=25)
     search_entry_widget.grid(row=0,column=1,padx=2,pady=2,sticky='we')
     search_entry_widget.focus_set()
     ignore_case_value=IntVar()
     Checkbutton(search_toplevel,text='Ignore Case',variable=ignore_case_value).grid(row=1,column=1,sticky='e',padx=2,pady=2)
-    Buttn(search_toplevel,text="Find All",underline=0,command=lambda: search_output(search_entry_widget.get(),ignore_case_value.get(),content_text,search_toplevel,search_entry_widget)).grid(row=0,column=2,sticky='e'+'w',padx=2,pady=2)
+    Button(search_toplevel,text="Find All",underline=0,command=lambda: search_output(search_entry_widget.get(),ignore_case_value.get(),content_text,search_toplevel,search_entry_widget)).grid(row=0,column=2,sticky='e'+'w',padx=2,pady=2)
     #to delete matched words highlight after using find functionality, we will overwrite close_search_window
     def close_search_window():
         content_text.tag_remove('match','1.0',END)
         search_toplevel.destroy()
-        search_toplevel.protocol('WM_DELETE_WINDOW',close_search_window)
-        return "break"
-    def search_output(needle,if_ignore_case,content_text,search_toplevel,search_box):
-        content_text.tag_remove('match','1.0',END)
-        matches_found=0
-        if needle:
-            """COMPLETE THIS PART LATER"""
+    search_toplevel.protocol('WM_DELETE_WINDOW',close_search_window)
+    return "break"
+def search_output(needle,if_ignore_case,content_text,search_toplevel,search_box):
+    content_text.tag_remove('match','1.0',END)
+    matches_found=0
+    if needle:
+        start_pos='1.0'
+        while True:
+            start_pos=content_text.search(needle,start_pos,nocase=if_ignore_case,stopindex=END)
+            if not start_pos:
+                break
+            end_pos='{}+{}c'.format(start_pos,len(needle))
+            content_text.tag_add('match',start_pos,end_pos)
+            matches_found+=1
+            start_pos=end_pos
+        content_text.tag_config('match',foreground='red',background='yellow')
+    search_box.focus_set()
+    search_toplevel.title('{} matches found'.format(matches_found))
 PROGRAM_NAME="Footprint Editor"
 root=Tk()
 root.geometry('350x350')
@@ -123,8 +134,8 @@ content_text.bind('<Control-y>',redo)# event binding for providing redo function
 content_text.bind('<Control-Y>',redo) # redo has different conditions in tkinter so we need to do thid in this way
 content_text.bind('<Control-a>',select_all)
 content_text.bind('<Control-A>',select_all)
-control_text.bind('<Control-f>',find_text)
-control_text.bind('<Control-F>',find_text)
+content_text.bind('<Control-f>',find_text)
+content_text.bind('<Control-F>',find_text)
 content_text.pack(expand='yes',fill='both')
 scroll_bar=Scrollbar(content_text)
 content_text.configure(yscrollcommand=scroll_bar.set)
