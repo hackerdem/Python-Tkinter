0216f5c7667bfcf77c7d564dc20ae2f0a8e7e549
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-10-27 18:11:32 +1100

    functions

--- text_editor.py
+++ text_editor.py
@@ -26,6 +26,7 @@ def open_file(event=None):
         content_text.delete(1.0,END)
         with open(file_name) as _file:
             content_text.insert(1.0,_file.read())
+    on_content_changed()
 def save(event=None):
     global file_name
     if not file_name:
@@ -55,16 +56,55 @@ def new_file(event=None):
     content_text.delete(1.0,END)
 def cut():
     content_text.event_generate("<<Cut>>")
+    on_content_changed()
+    return 'break'
+def paste():
+    content_text.event_generate("<<Paste>>")
+    on_content_changed()
+    return 'break'
+def copy():
+    content_text.event_generate("<<Copy>>")
+    on_content_changed()
     return 'break'
 def undo():
     content_text.event_generate("<<Undo>>")
+    on_content_changed()
     return 'break'
 def redo(event=None):
     content_text.event_generate("<<Redo>>")
+    on_content_changed()
     return 'break' 
 def select_all(event=None):
     content_text.tag_add('sel','1.0','end')
     return "break"
+def highlight_line(interval=100):
+    content_text.tag_remove("active_line",1.0,"end")
+    content_text.tag_add("active_line","insert linestart","insert lineend+1c")
+    content_text.after(interval,toggle_highlight)
+def undo_highlight():
+    content_text.tag_remove("active_line",1.0,"end")
+def toggle_highlight(event=None):
+    if to_highlight_line.get():
+        highlight_line()
+    else:
+        undo_highlight()
+    
+def get_line_numbers(evet=None):
+    output=''
+    if show_line_number.get():
+        row,col=content_text.index("end").split(".")
+        for i in range(1,int(row)):
+            output+=str(i)+'\n'
+    return output
+def update_line_numbers(event=None):
+    line_numbers=get_line_numbers()
+    line_number_bar.config(state='normal')
+    line_number_bar.delete('1.0','end')
+    line_number_bar.insert('1.0',line_numbers)
+    line_number_bar.config(state='disabled')
+    
+def on_content_changed(event=None):
+    update_line_numbers()
 def find_text(event=None):
     search_toplevel=Toplevel(root)
     search_toplevel.title('Find Text')
@@ -107,7 +147,13 @@ new_file_icon=PhotoImage(file='icons/new_file.gif')
 saveas_file_icon=PhotoImage(file='icons/save.gif')
 save_icon=PhotoImage(file='icons/save.gif')
 open_file_icon=PhotoImage(file='icons/open_file.gif')
-
+undo_icon=PhotoImage(file='icons/undo.gif')
+redo_icon=PhotoImage(file='icons/redo.gif')
+cut_icon=PhotoImage(file='icons/cut.gif')
+copy_icon=PhotoImage(file='icons/copy.gif')
+paste_icon=PhotoImage(file='icons/paste.gif')
+find_icon=PhotoImage(file='icons/find_text.gif')
+about_icon=PhotoImage(file='icons/about.gif')
 """ Color options are listed in a dictionary"""
 color_shemes={
     'Default': '#000000.#FFFFFF',
@@ -133,14 +179,14 @@ menu_bar.add_cascade(label='File',menu=file_menu)
 
 #edit menu
 edit_menu=Menu(menu_bar,tearoff=0)
-edit_menu.add_command(label='Undo',accelerator='Ctrl+Z',command=undo,compound='left',image='',underline=0)
-edit_menu.add_command(label='Redo',accelerator='Ctrl+Y',command=redo,compound='left',image='',underline=0)
+edit_menu.add_command(label='Undo',accelerator='Ctrl+Z',command=undo,compound='left',image=undo_icon,underline=0)
+edit_menu.add_command(label='Redo',accelerator='Ctrl+Y',command=redo,compound='left',image=redo_icon,underline=0)
 edit_menu.add_separator()
-edit_menu.add_command(label='Cut',accelerator='Ctrl+X',command=cut,compound='left',image='',underline=0)
-edit_menu.add_command(label='Copy',accelerator='Ctrl+C',compound='left',image='',underline=0)
-edit_menu.add_command(label='Paste',accelerator='Ctrl+V',compound='left',image='',underline=0)
+edit_menu.add_command(label='Cut',accelerator='Ctrl+X',command=cut,compound='left',image=cut_icon,underline=0)
+edit_menu.add_command(label='Copy',accelerator='Ctrl+C',command=copy,compound='left',image=copy_icon,underline=0)
+edit_menu.add_command(label='Paste',accelerator='Ctrl+V',command=paste,compound='left',image=paste_icon,underline=0)
 edit_menu.add_separator()
-edit_menu.add_command(label='Find',accelerator='Ctrl+F',command=find_text,compound='left',image='',underline=0)
+edit_menu.add_command(label='Find',accelerator='Ctrl+F',command=find_text,compound='left',image=find_icon,underline=0)
 file_menu.add_separator()
 edit_menu.add_command(label='Select All',accelerator='Ctrl+A',command=select_all,compound='left',image='',underline=7)
 menu_bar.add_cascade(label='Edit',menu=edit_menu)
@@ -153,8 +199,8 @@ view_menu.add_checkbutton(label="Show Line Number",variable=show_line_number)
 show_cursor_info=IntVar()
 show_cursor_info.set(1)
 view_menu.add_checkbutton(label="Show Cursor Location at Bottom",variable=show_cursor_info)
-highlight_line=IntVar()
-view_menu.add_checkbutton(label="Highlight Current Line",onvalue=1,offvalue=0,variable=highlight_line)
+to_highlight_line=IntVar()
+view_menu.add_checkbutton(label="Highlight Current Line",command=toggle_highlight,onvalue=1,offvalue=0,variable=to_highlight_line)
 themes_menu=Menu(menu_bar,tearoff=0)
 view_menu.add_cascade(label="Themes",menu=themes_menu)
 theme_choice=StringVar()
@@ -166,7 +212,7 @@ menu_bar.add_cascade(label='View',menu=view_menu)
 
 #about menu
 about_menu=Menu(menu_bar,tearoff=0)
-about_menu.add_command(label='About',command=display_about_messagebox,compound='left',underline=0)
+about_menu.add_command(label='About',command=display_about_messagebox,compound='left',image=about_icon,underline=0)
 about_menu.add_command(label='Help',command=display_help_messagebox,compound='left',image='',underline=0)
 menu_bar.add_cascade(label='About',menu=about_menu)
 root.config(menu=menu_bar)
@@ -179,8 +225,18 @@ shortcut_bar.pack(expand='no',fill='x')
 line_number_bar=Text(root,width=4,padx=3,takefocus=0,border=0,background='#778899',state='disabled',wrap='none')
 line_number_bar.pack(side='left',fill='y')
 
+#shortcut menu
+icons=('new_file','open_file','save','cut','copy','paste','undo','redo','find_text')
+for i,icon in enumerate(icons):
+    tool_bar_icon=PhotoImage(file='icons/{}.gif'.format(icon))
+    cmd=eval(icon)
+    tool_bar=Button(shortcut_bar,image=tool_bar_icon,command=cmd)
+    tool_bar.image=tool_bar_icon
+    tool_bar.pack(side='left')
+
 #text and scroolbar widgets for root window
 content_text=Text(root,wrap='word',undo=1,background='#778899',font=("Helvetica", 16),fg="#FFFFFF")
+content_text.tag_configure('active_line',background='#000000')
 content_text.bind('<Control-y>',redo)# event binding for providing redo functionality
 content_text.bind('<Control-Y>',redo) # redo has different conditions in tkinter so we need to do thid in this way
 content_text.bind('<Control-a>',select_all)
@@ -194,6 +250,7 @@ content_text.bind('<Control-o>', open_file)
 content_text.bind('<Control-S>', save)
 content_text.bind('<Control-s>', save)
 content_text.bind('<KeyPress-F1>',display_help_messagebox)
+content_text.bind('<Any-KeyPress>',on_content_changed)
 content_text.pack(expand='yes',fill='both')
 scroll_bar=Scrollbar(content_text)
 content_text.configure(yscrollcommand=scroll_bar.set)
